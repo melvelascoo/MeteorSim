a9f50d729f1384d90ce70aa4ab75b1b3c363dc7e
Melissa Velasco <mel.velascoo@gmail.com>
2025-10-05 00:24:20 -0400

    Agregando carpeta docs para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/MeteorSim/', // 👈 importante para GitHub Pages
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
